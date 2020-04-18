08c197a02c907e12ce7ec2d35019bdc3d9fd29ae
davidkartuzinski <david@kaidawei.me>
2020-04-19 01:14:31 +0200

    fixed the button accessibility issue

--- base-apparel-app/src/components/EmailInput.js
+++ base-apparel-app/src/components/EmailInput.js
@@ -120,7 +120,7 @@ const EmailInput = () => {
         />
       </label>
 
-      <button type="submit" name="submit" disable={disable}>
+      <button type="submit" name="submit" disable={disable} role="button">
         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
           <path
             fill="none"
